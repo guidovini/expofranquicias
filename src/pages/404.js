7f1497ba9387a30e26aecda985e29c8744029bd2
Guido Santillan <guidosantillan01@gmail.com>
2019-06-13 15:35:41 -0500

    Change 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="title">Página no Encontrada</h1>
+    <p className="content">
+      Regresar al <Link to="/">inicio</Link>
+    </p>
   </Layout>
 )
 
