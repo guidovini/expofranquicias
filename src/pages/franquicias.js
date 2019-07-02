a58da6bd04a27e3ae74ff975ed75bea3c122ca07
Guido Santillan <guidosantillan01@gmail.com>
2019-07-02 18:43:24 -0500

    Update franquicias.js file with getData function

--- src/pages/franquicias.js
+++ src/pages/franquicias.js
@@ -1,41 +1,26 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+
+import getData from '../data/getData'
 
 const ListadoFranquicias = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allGoogleSheetListadoFranquiciasRow {
-        nodes {
-          id
-          nombre
-          categoria
-          concepto
-          aniosOperacion
-          numeroLocales
-          tamanoLocal
-          disponibilidadTerritorial
-          paisOrigen
-          tiempoRetornoInversion
-          inversion
-        }
-      }
-    }
-  `)
-
-  const franquicias = data.allGoogleSheetListadoFranquiciasRow.nodes
+  const { franquicias, listaPaises } = getData()
+  // console.log(franquicias, listaPaises)
 
   return (
-    <div>
-      <h1>Franquicias Disponibles</h1>
-      {franquicias.map(franquicia => {
-        console.log(franquicia.id)
-        return (
-          <div key={franquicia.id}>
-            <p>{franquicia.nombre}</p>
-            <p>{franquicia.inversion}</p>
-          </div>
-        )
-      })}
+    <div className="section">
+      <h1 className="title">Franquicias Disponibles</h1>
+      <div className="container">
+        {franquicias.map(franquicia => {
+          return (
+            <div key={franquicia.id || franquicia.nombre} className="box">
+              <p className="title is-5">{franquicia.nombre}</p>
+              <p className="content">{franquicia.categoria}</p>
+              <p className="content">{franquicia.paisOrigen}</p>
+              <p className="content">{franquicia.inversion}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
