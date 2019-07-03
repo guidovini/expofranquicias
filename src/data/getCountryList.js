2bad044f995d700b08f7615c13b8d1593e423a50
Guido Santillan <guidosantillan01@gmail.com>
2019-07-03 00:27:40 -0500

    Sort countryList

--- src/data/getCountryList.js
+++ src/data/getCountryList.js
@@ -3,7 +3,12 @@ const getCountryList = franchises => {
   franchises.map(franchise => {
     return list.push(franchise.paisOrigen)
   })
-  const countryList = list.filter((item, index) => list.indexOf(item) === index)
+  const countryList = list
+    .filter((item, index) => list.indexOf(item) === index)
+    .sort((a, b) => {
+      if (a < b) return -1
+      if (a > b) return 1
+    })
   // console.log('countryList', countryList)
   return countryList
 }
