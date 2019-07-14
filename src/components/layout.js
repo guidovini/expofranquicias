d695547b86710b6ff2099b020017dc60ee9ad03b
Guido Santillan <guidosantillan01@gmail.com>
2019-07-14 04:19:28 -0500

    Separate GraphQL query into a new varible

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,17 +15,19 @@ import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
 import Footer from './footer'
 
+const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={query}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
