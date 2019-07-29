b316faeaf4d7a50892a112280ee0d519bbb5b81c
Guido Santillan <guidosantillan01@gmail.com>
2019-07-29 12:51:49 -0500

    Component renders placeholder when no image

--- src/components/FranchiseItemLogo.js
+++ src/components/FranchiseItemLogo.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 // Components
 import Logo from './logo'
-import Image from './image'
 
 // Styles
 import franchiseItemStyles from './franchiseItem.module.scss'
@@ -10,11 +9,7 @@ import franchiseItemStyles from './franchiseItem.module.scss'
 const FranchiseItemLogo = ({ nombre }) => {
   return (
     <div className={franchiseItemStyles.imageBox} data-testid="franchise-logo">
-      {nombre ? (
-        <Logo imgName={`${nombre}.jpg`} />
-      ) : (
-        <Logo imgName={`franchise.png`} />
-      )}
+      <Logo imgName={`${nombre}.jpg`} />
     </div>
   )
 }
