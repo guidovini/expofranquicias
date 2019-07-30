55625b2c41286c9e4ade885fda89dbb438ff3ce4
Guido Santillan <guidosantillan01@gmail.com>
2019-07-30 00:57:08 -0500

    Config vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,9 +22,6 @@ module.exports = {
           type: 'service_account',
           project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
           private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-          private_key: (activeEnv = 'development'
-            ? process.env.GOOGLE_SHEETS_PRIVATE_KEY
-            : JSON.parse(process.env.GOOGLE_SHEETS_PRIVATE_KEY)),
           client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
           client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
           auth_uri: 'https://accounts.google.com/o/oauth2/auth',
@@ -32,6 +29,11 @@ module.exports = {
           auth_provider_x509_cert_url:
             'https://www.googleapis.com/oauth2/v1/certs',
           client_x509_cert_url: process.env.GOOGLE_SHEETS_CLIENT_CERT_URL,
+          ...(activeEnv = 'development'
+            ? { private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY }
+            : {
+                private_key: JSON.parse(process.env.GOOGLE_SHEETS_PRIVATE_KEY),
+              }),
         },
       },
     },
