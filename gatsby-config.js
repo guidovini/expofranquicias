a621c2c4bb633945d1d23edaa6d02e6cbcbf9d52
Guido Santillan <guidosantillan01@gmail.com>
2019-07-30 01:04:48 -0500

    Config vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,10 @@ module.exports = {
           auth_provider_x509_cert_url:
             'https://www.googleapis.com/oauth2/v1/certs',
           client_x509_cert_url: process.env.GOOGLE_SHEETS_CLIENT_CERT_URL,
-          ...(activeEnv = 'development'
-            ? { private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY }
+          ...(process.env.LOCAL_ENV
+            ? {
+                private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+              }
             : {
                 private_key: JSON.parse(process.env.GOOGLE_SHEETS_PRIVATE_KEY),
               }),
